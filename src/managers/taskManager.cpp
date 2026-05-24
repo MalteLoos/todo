bd02f0c2938095890ae815dad340c24bc1984eb9
Jonida-Zekaj <jonidazekaj04@gmail.com>
2026-05-24 15:06:51 +0200

    Operation functions declared./Ex2

--- src/managers/taskManager.cpp
+++ src/managers/taskManager.cpp
@@ -4,9 +4,36 @@
 
 #include "taskManager.h"
 
-// Implementation of TaskManager class ..?!
+// OPERATIONS UPON TASKS
+void TaskManager::addTask(const Task& task){
+    tasks[task.getId()] = task;
+}
 
-// here operations 
+bool TaskManager::deleteTask(const std::string& id){
+    return tasks.erase(id) > 0; // => erase =[0, 1] no. of tasks deleted
+}
+
+bool TaskManager::updateTask(const std::string& id, const Task& updatedTask){
+    auto iterator = tasks.find(id); // => task with given id was:
+    if (iterator != tasks.end()){
+        iterator->second = updatedTask;
+        return true; // => a) found
+    } 
+    return false; // => b) not found
+}
+
+bool TaskManager::finishTask(const std::string& id){
+    auto iterator = tasks.find(id);
+    if (iterator != tasks.end()){
+        iterator -> second.setCompleted(true);
+        return true;
+    
+    }
+    return false;
+}
+
+
+
 
 
 // here algorithms -- > searching, sorting, filtering 
