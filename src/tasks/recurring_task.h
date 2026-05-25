cb5e0639de8526f10ecab277a5b9315ef8b8b7b9
Jonida-Zekaj <jonidazekaj04@gmail.com>
2026-05-25 15:05:46 +0200

    designed recurringTask subclass.

--- src/tasks/recurring_task.h
+++ src/tasks/recurring_task.h
@@ -5,9 +5,35 @@
 
 class recurringTask : public Task { // => recurringTask is subclass to Task superclass
     private:
+    Recurrence recurrence; // => pattern of repetition
+    std::chrono::system_clock::time_point nextOcurrence; // => next scheduling 
 
     public:
+    // CONSTRUCTOR
+    recurringTask();
+    recurringTask(const std::string& title,
+                const std::chrono::system_clock::time_point& deadline,
+                Priority prioriity,
+                Recurrence recurrence);
 
-}
+    // GETTERS 
+    Recurrence getRecurrence() const;
+    std::chrono::system_clock::time_point getNextOccurrence() const;
+
+
+    //SETTERS
+    void setRecurrence(Recurrence recurrence);
+
+
+    // UTILITIES
+    void markCompleted(); 
+    std::string toString() const override;
+    std::string getType() const override;
+};
+
+private: 
+    std::chrono::system_clock::time_point calculateNextOcurrence(const std::chrono::system_clock::time_point& getCurrentDeadline() const);
+};
+
 
 #endif //RECURRING_TASK
