90443569fbd52e661e2bbcbac7b1ed7131857d49
Jonida-Zekaj <jonidazekaj04@gmail.com>
2026-05-25 21:25:38 +0200

    Getters for subclasses.

--- src/managers/taskManager.h
+++ src/managers/taskManager.h
@@ -27,13 +27,16 @@ public:
 
     std::vector<std::unique_ptr<Task>> searchByTitle(const std::string& keyword) const; // => searching tasks whose title contains a specific >> keyword<<
 
-    std::vector<std::unique_ptr<Task>> filterByPriority(Priority priority) const; // => filtering tasks based on priority leve
-    std::vector<std::unique_ptr<Task>> filterByStatus(bool completed) const; // => filtering tasks aka. get the completed or incomplted ones
-    std::vector<std::unique_ptr<Task>> getOverdueTasks() const; // => retrieve tasks past deadline 
+    std::vector<Task*> filterByPriority(Priority priority) const; // => filtering tasks based on priority leve
+    std::vector<Task*> filterByStatus(bool completed) const; // => filtering tasks aka. get the completed or incomplted ones
+    std::vector<Task*> getOverdueTasks() const; // => retrieve tasks past deadline 
 
-    std::vector<std::unique_ptr<Task>> getTasksSortedByDeadline() const; // => sorting tasks by their deadlines
-    std::vector<std::unique_ptr<Task>> getTasksSortedByPriority() const; // => sort: highest to lowest 
+    std::vector<Task*> getTasksSortedByDeadline() const; // => sorting tasks by their deadlines
+    std::vector<Task*> getTasksSortedByPriority() const; // => sort: highest to lowest 
 
+    std::vector<Task*> getRecurringTasks() const; // => retrieve all repeated tasks
+    std::vector<Task*> getTimedTasks() const; // => retrieved all tasks w/ timer
+    
     //UTILITIES
     size_t getTaskCount() const; 
     void displayAllTasks() const;
