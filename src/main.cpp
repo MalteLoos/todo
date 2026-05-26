0796d7ed1cadc65a5d58c1a88c59ab943158288c
Jonida-Zekaj <jonidazekaj04@gmail.com>
2026-05-26 15:04:40 +0200

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include "tasks/task.h"
-#include "managers/taskManager.h"
+#include <memory>
+#include <chrono>
 
-TaskManager manager; 
+#include "managers/taskManager.h"
+#include "tasks/task.h"
+#include "tasks/timed_task.h"
+#include "tasks/recurring_task.h"
+#include "storage/taskStorage.h"
 
 int main() {
-    std::cout << "Todo Application\n";
-    return 0;
+    TaskManager manager; // => taskManager instance 
+
+    // TIME SPECIFICTIONS 
+    auto now = std::chrono::system_clock::now();
+    auto tomorrow = std::chrono::system_clock::hours(24);
+    auto nextWeek = std::chrono::system_clock::hours(24 * 7);
+
+    //CREATING TASKS
+    auto taskA = std::make_unique<Task>("Finish C++ Project", tomorrow, Priority::HIGH, Category::EDUCATION, Recurrence::NONE);
+    taskA -> setDescription("Final debugging and testing, then submit.");
+    manager.addTask(std::move(taskA));
+
 }
